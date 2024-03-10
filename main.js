9e317f735fdeec6a10aeedaf60674b74a5d11421
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2024-03-09 22:51:32 -0800

    Add playRound()
    - Take player's choice and computer's choice
    - Compare selections by seeing if the player's choice with the computer's
    choice is a victory case to determine a win vs loss vs tie

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+// Choose computer's choice at random then return the choice as a string
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    switch (Math.floor(Math.random() * (Math.floor(3) /* Max */ - Math.ceil(1) /* Min */ + 1) + Math.ceil(1) /* Min */)) {
+    const choices = ["rock", "paper", "scissors"];
+    switch (
+        Math.floor(
+            Math.random() *
+                (Math.floor(3) /* Max */ - Math.ceil(1) /* Min */ + 1) +
+                Math.ceil(1) /* Min */
+        )
+    ) {
         case 1:
             return choices[0];
         case 2:
@@ -10,4 +17,25 @@ function getComputerChoice() {
     }
 }
 
-choice = getComputerChoice();
+// Take choices as strings and return the results
+function playRound(playerSelection, computerSelection) {
+    const victories = {
+        rock: ["scissors"],
+        paper: ["rock"],
+        scissors: ["paper"],
+    };
+
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    } else if (victories[playerSelection].includes(computerSelection)) {
+        return "Win";
+    } else {
+        return "Loss";
+    }
+}
+
+const playerChoice = prompt(
+    'Choose "Rock", "Paper" or "Scissors":'
+).toLowerCase();
+const computerChoice = getComputerChoice();
+const result = playRound(playerChoice, computerChoice);
